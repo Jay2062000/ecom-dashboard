b2c320446ead11e65a1ef5366a42152fd7b2e23d
Jay Nimje <nimjejay2@gmail.com>
2023-02-27 10:48:01 +0530

    first Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ecom Project</h1>
+      <button>Normal Button</button>
+      <Button>Bootstrap Button</Button>
+    </div>
+  );
+}
+
+export default App;
